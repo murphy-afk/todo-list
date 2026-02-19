1a742547137fdf0fc09257e015bdac82e2ee1b2f
murphy-afk <bardarenoemi@gmail.com>
2026-02-19 16:27:57 +0100

    feat: tailwind and fake set completed button

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,8 +1,37 @@
-export default function ListItem ({item}) {
-  return(
-    <li>
-      <p>{item.title}</p>
-      <p>{item.priority}</p>
+import { useState } from "react";
+
+export default function ListItem({ item }) {
+  const [completed, setCompleted] = useState(item.completed);
+  const priorityStyles = {
+    Low: "bg-green-700/40 text-green-300 border-green-500/40",
+    Medium: "bg-yellow-700/40 text-yellow-300 border-yellow-500/40",
+    High: "bg-red-700/40 text-red-300 border-red-500/40"
+  };
+
+  return (
+    <li className="bg-gray-800 border border-gray-700 rounded-lg p-5 shadow-md hover:shadow-teal-500/20 hover:border-teal-400 transition-all">
+
+      <div className="flex justify-between items-start mb-2">
+        <h3 className="text-xl font-semibold text-teal-200">{item.title}</h3>
+
+        <span className={`text-xs px-3 py-1 rounded-full border ${priorityStyles[item.priority]}`} >
+          Priority {item.priority}
+        </span>
+      </div>
+
+      <p className="text-gray-300 mb-3">{item.description}</p>
+
+      <div className="flex justify-between items-center text-sm text-gray-400">
+        <p>
+          <span className="font-medium text-gray-300">Deadline:</span>{item.deadline}
+        </p>
+
+        <p className={`font-semibold ${completed ? "text-green-400" : "text-yellow-400"}`} >
+          {completed ? "Completed" : <button 
+          className="px-3 py-1 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition-colors"
+          onClick={(e) => setCompleted(true)}>Mark as Done</button>}
+        </p>
+      </div>
     </li>
-  )
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,29 @@
 import { useEffect, useState } from "react"
 import ListItem from "../components/ListItem"
 import axios from "axios"
-// import { todos } from "../data"
 
 export default function Home() {
   const [todos, setTodos] = useState([])
+
   useEffect(() => {
-    axios.get("http://localhost:3000/todos")
-    .then((res) => setTodos(res.data))
+    axios.get(import.meta.env.VITE_BACKEND_URL)
+      .then((res) => setTodos(res.data))
   }, [])
 
   return (
-    <>
-      <h1>Home</h1>
-      <h2>To do list</h2>
-      <ul>
-        {todos.map(todo => <ListItem item={todo} key={todo.id} />)}
-      </ul>
-    </>
+    <div className="min-h-screen bg-gray-900 text-white px-6 py-10">
+      <div className="max-w-3xl mx-auto">
+
+        <h1 className="text-2xl font-semibold mb-8 text-center text-gray-300">
+          To-Do List
+        </h1>
+
+        <ul className="space-y-4">
+          {todos.map(todo => (
+            <ListItem item={todo} key={todo.id} />
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
